d6818536c3b2d3dac87f176693546e106ba11226
Yesssung <cjsrnr0504@naver.com>
2024-04-26 12:17:21 +0900

    Create Practice02.sql
    24.04.26 학원

---
+++ Practice02.sql
@@ -0,0 +1,20 @@
+-- 문제 1.
+
+-- 문제 2.
+
+-- 문제 3.
+
+-- 문제 4.
+SELECT max_salary, min_salary FROM jobs  ORDER BY department_id DESC;
+
+-- 문제 5.
+
+-- 문제 6.
+
+-- 문제 7.
+
+-- 문제 8.
+
+-- 문제 9.
+
+-- 문제 10.
